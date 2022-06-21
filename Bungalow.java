44c18dfe167ea4ab31e69fc5ce5f101ece7cb5f5
Franzi784 <102363294+Franzi784@users.noreply.github.com>
2022-06-21 21:55:23 +0200

    Add files via upload

--- Bungalow.java
+++ Bungalow.java
@@ -16,6 +16,7 @@ import java.awt.Color;
 import java.awt.SystemColor;
 import com.jgoodies.forms.factories.DefaultComponentFactory;
 import javax.swing.JSpinner;
+import javax.swing.JTextField;
 import javax.swing.UIManager;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -23,6 +24,8 @@ import java.awt.event.ActionEvent;
 public class Bungalow extends JFrame {
 
 	private JPanel contentPane;
+	public boolean buttonJetztBuchen = false;
+	public boolean buttonJHierStonieren = false;
 
 	/**
 	 * Launch the application.
@@ -42,6 +45,10 @@ public class Bungalow extends JFrame {
 
 	/**
 	 * Create the frame.
+	 * //textBungalowNr = new JTextField();
+		textBungalowNr.setBounds(159, 21, 164, 40);
+		getContentPane().add(textBungalowNr);
+		textBungalowNr.setColumns(10);
 	 */
 	public Bungalow() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -52,12 +59,14 @@ public class Bungalow extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		//Hintergrund Verfuegbarkeit
 		Panel VerfuegbarkeitsPanel = new Panel();
 		VerfuegbarkeitsPanel.setBackground(Color.LIGHT_GRAY);
-		VerfuegbarkeitsPanel.setBounds(89, 48, 321, 562);
+		VerfuegbarkeitsPanel.setBounds(84, 48, 321, 562);
 		contentPane.add(VerfuegbarkeitsPanel);
 		VerfuegbarkeitsPanel.setLayout(null);
 		
+		//Ueberschrifften
 		JLabel lblVerfuegbarkeit = new JLabel("Verfuegbarkeit:");
 		lblVerfuegbarkeit.setBounds(94, 10, 125, 22);
 		lblVerfuegbarkeit.setFont(new Font("Tahoma", Font.PLAIN, 18));
@@ -73,14 +82,18 @@ public class Bungalow extends JFrame {
 		lblAbreiseLable.setBounds(54, 110, 63, 22);
 		VerfuegbarkeitsPanel.add(lblAbreiseLable);
 		
-		JTextPane txtpnAnreise = new JTextPane();
-		txtpnAnreise.setBounds(112, 67, 52, 22);
-		VerfuegbarkeitsPanel.add(txtpnAnreise);
+		//eingabefeldAnreiseUndAbreise
+		JTextPane txtpnWoche1 = new JTextPane();
+		txtpnWoche1.setBounds(112, 67, 52, 22);
+		VerfuegbarkeitsPanel.add(txtpnWoche1);
+		getContentPane().add(txtpnWoche1);
 		
-		JTextPane textPane = new JTextPane();
-		textPane.setBounds(112, 110, 52, 22);
-		VerfuegbarkeitsPanel.add(textPane);
+		JTextPane textWoche = new JTextPane();
+		textWoche.setBounds(112, 110, 52, 22);
+		VerfuegbarkeitsPanel.add(textWoche);
+		getContentPane().add(textWoche);
 		
+		//Ueberschrifften
 		JLabel lblWoche = new JLabel("Woche");
 		lblWoche.setBounds(112, 56, 45, 13);
 		VerfuegbarkeitsPanel.add(lblWoche);
@@ -89,6 +102,7 @@ public class Bungalow extends JFrame {
 		lblWoche_1.setBounds(112, 99, 45, 13);
 		VerfuegbarkeitsPanel.add(lblWoche_1);
 		
+		//eingaben
 		JTextPane txtpnJahr = new JTextPane();
 		txtpnJahr.setBounds(174, 67, 52, 22);
 		VerfuegbarkeitsPanel.add(txtpnJahr);
@@ -96,6 +110,7 @@ public class Bungalow extends JFrame {
 		JTextPane textJahr_1 = new JTextPane();
 		textJahr_1.setBounds(174, 110, 52, 22);
 		VerfuegbarkeitsPanel.add(textJahr_1);
+		getContentPane().add(textJahr_1);
 		
 		JLabel lblJahr = new JLabel("Jahr");
 		lblJahr.setBounds(174, 56, 45, 13);
@@ -105,19 +120,23 @@ public class Bungalow extends JFrame {
 		lblJahr_1.setBounds(174, 99, 45, 13);
 		VerfuegbarkeitsPanel.add(lblJahr_1);
 		
+		//UeberschriftFreieBungalows
 		JLabel lblFreiebungalows = new JLabel("freie Bungalows:");
 		lblFreiebungalows.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		lblFreiebungalows.setBounds(78, 161, 152, 36);
 		VerfuegbarkeitsPanel.add(lblFreiebungalows);
 		
+		
 		JTextPane textFreieBungalows = new JTextPane();
 		textFreieBungalows.setEditable(false);
-		textFreieBungalows.setBounds(54, 215, 248, 312);
+		textFreieBungalows.setBounds(28, 207, 274, 320);
 		VerfuegbarkeitsPanel.add(textFreieBungalows);
+		getContentPane().add(textFreieBungalows);
 		
+		//Hintergrund 
 		Panel BuchungsPanel = new Panel();
 		BuchungsPanel.setBackground(Color.LIGHT_GRAY);
-		BuchungsPanel.setBounds(462, 144, 369, 371);
+		BuchungsPanel.setBounds(439, 145, 369, 371);
 		contentPane.add(BuchungsPanel);
 		BuchungsPanel.setLayout(null);
 		
@@ -139,18 +158,22 @@ public class Bungalow extends JFrame {
 		JTextPane textWochePane_1 = new JTextPane();
 		textWochePane_1.setBounds(105, 46, 52, 22);
 		BuchungsPanel.add(textWochePane_1);
+		getContentPane().add(textWochePane_1);
 		
 		JTextPane textWochePane_2 = new JTextPane();
 		textWochePane_2.setBounds(105, 88, 52, 22);
 		BuchungsPanel.add(textWochePane_2);
+		getContentPane().add(textWochePane_2);
 		
 		JTextPane textJahrPane_1 = new JTextPane();
 		textJahrPane_1.setBounds(209, 46, 52, 22);
 		BuchungsPanel.add(textJahrPane_1);
+		getContentPane().add(textJahrPane_1);
 		
 		JTextPane textJahrPane_2 = new JTextPane();
 		textJahrPane_2.setBounds(208, 88, 53, 22);
 		BuchungsPanel.add(textJahrPane_2);
+		getContentPane().add(textJahrPane_2);
 		
 		JLabel lblWoche_2 = new JLabel("Woche:");
 		lblWoche_2.setBounds(105, 35, 45, 13);
@@ -173,9 +196,11 @@ public class Bungalow extends JFrame {
 		lblBuchungenMaximaljahre.setBounds(87, 101, 244, 47);
 		BuchungsPanel.add(lblBuchungenMaximaljahre);
 		
+		//ButtonJetztBuchen
 		JButton btnJetztBuchen = new JButton("Jetzt Buchen");
 		btnJetztBuchen.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				buttonJetztBuchen = true;
 			}
 		});
 		btnJetztBuchen.setBackground(UIManager.getColor("Button.darkShadow"));
@@ -183,13 +208,17 @@ public class Bungalow extends JFrame {
 		btnJetztBuchen.setBounds(109, 301, 168, 39);
 		BuchungsPanel.add(btnJetztBuchen);
 		
+		
+		
 		JTextPane textAnzahlPersonenPane_1 = new JTextPane();
 		textAnzahlPersonenPane_1.setBounds(177, 190, 52, 22);
 		BuchungsPanel.add(textAnzahlPersonenPane_1);
+		getContentPane().add(textAnzahlPersonenPane_1);
 		
-		JTextPane txtpnBungalow = new JTextPane();
-		txtpnBungalow.setBounds(177, 156, 52, 24);
-		BuchungsPanel.add(txtpnBungalow);
+		JTextPane txtpnBungalowNummer = new JTextPane();
+		txtpnBungalowNummer.setBounds(177, 156, 52, 24);
+		BuchungsPanel.add(txtpnBungalowNummer);
+		getContentPane().add(txtpnBungalowNummer);
 		
 		JLabel lblBungalow = new JLabel("Bungalow:");
 		lblBungalow.setFont(new Font("Tahoma", Font.PLAIN, 14));
@@ -206,6 +235,7 @@ public class Bungalow extends JFrame {
 		BuchungsPanel.add(lblBuchungsnummer);
 		lblBuchungsnummer.setFont(new Font("Tahoma", Font.BOLD, 14));
 		
+		//Buchungsnummer
 		JTextPane txtpnBuchungsnummer = new JTextPane();
 		txtpnBuchungsnummer.setEditable(false);
 		txtpnBuchungsnummer.setBounds(99, 262, 178, 29);
@@ -213,7 +243,7 @@ public class Bungalow extends JFrame {
 		
 		Panel InformationPanel = new Panel();
 		InformationPanel.setBackground(Color.LIGHT_GRAY);
-		InformationPanel.setBounds(861, 48, 321, 556);
+		InformationPanel.setBounds(843, 48, 321, 556);
 		contentPane.add(InformationPanel);
 		InformationPanel.setLayout(null);
 		
@@ -226,6 +256,7 @@ public class Bungalow extends JFrame {
 		textStatusPane_1.setEditable(false);
 		textStatusPane_1.setBounds(37, 231, 258, 233);
 		InformationPanel.add(textStatusPane_1);
+		getContentPane().add(textStatusPane_1);
 		
 		JLabel lblStatus = new JLabel("Status:");
 		lblStatus.setFont(new Font("Tahoma", Font.PLAIN, 14));
@@ -240,11 +271,19 @@ public class Bungalow extends JFrame {
 		JTextPane textBuchungsnummerPane_2 = new JTextPane();
 		textBuchungsnummerPane_2.setBounds(55, 134, 196, 31);
 		InformationPanel.add(textBuchungsnummerPane_2);
+		getContentPane().add(textBuchungsnummerPane_2);
 		
 		JButton btnHierStonieren = new JButton("Hier Stonieren");
+		btnHierStonieren.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				buttonJHierStonieren = true;
+			}
+		});
 		btnHierStonieren.setBounds(102, 488, 145, 36);
 		InformationPanel.add(btnHierStonieren);
 		btnHierStonieren.setFont(new Font("Tahoma", Font.BOLD, 14));
 		btnHierStonieren.setBackground(UIManager.getColor("Button.darkShadow"));
+		
+
 	}
 }
